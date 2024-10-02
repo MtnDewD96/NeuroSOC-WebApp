ba9ba5b9fd51cc22f41ecd3ca435be9749425e24
MtnDewD96 <marc.doyon16@gmail.com>
2024-10-02 18:53:11 -0400

    dynamic neural network

--- src/src/components/CustomNNVisualizer.js
+++ src/src/components/CustomNNVisualizer.js
@@ -3,27 +3,50 @@ import NeuralNetwork from './NeuralNetwork'
 export default function CustomNNVisualizer(props) {
   let windowWidth = props.width
   let windowHeight = props.height
-  const neurons = [
-    { x: 100, y: 100, voltage: -70 },
-    { x: 100, y: 250, voltage: -50 },
-    { x: 100, y: 400, voltage: 20 },
-    { x: 300, y: 100, voltage: -40 },
-    { x: 300, y: 250, voltage: 10 },
-    { x: 300, y: 400, voltage: 30 },
-  ]
+  let layers = props.layers.layers
 
-  // Example connections
-  const connections = [
-    { from: 0, to: 3, thickness: 1 },
-    { from: 1, to: 4, thickness: 2 },
-    { from: 2, to: 5, thickness: 3 },
-    { from: 0, to: 4, thickness: 1.5 },
-    { from: 1, to: 3, thickness: 2.5 },
-    { from: 2, to: 4, thickness: 2 },
-  ]
+  let neurons = []
+  let connections = []
+  let x_offset = 100
+  let y_offset = 100
+  let x_step = (windowWidth - 200) / layers.length
+
+  layers.forEach((layer) => {
+    let layer_step = (windowHeight - 100) / layer.unit
+    y_offset = windowHeight / layer.unit / 2
+    for (let i = 0; i < layer.unit; i++) {
+      neurons.push({ x: x_offset, y: y_offset + i * layer_step, voltage: -70 })
+    }
+    x_offset += x_step
+  })
+
+  let status = 0
+  let connections_made = 0
+  for (let i = 0; i < layers.length; i++) {
+    connections_made += layers[i].unit
+    console.log(i)
+    if (layers[i + 1] != null) {
+      for (let j = 0; j < layers[i].unit; j++) {
+        console.log('---', j)
+        for (let k = 0; k < layers[i + 1].unit; k++) {
+          console.log('------', status + j, connections_made + k)
+          connections.push({ from: status + j, to: connections_made + k, thickness: 1 })
+        }
+      }
+    } else {
+      break
+    }
+    status += layers[i].unit
+  }
+
   return (
     <>
-      <NeuralNetwork neurons={neurons} connections={connections} />
+      <NeuralNetwork
+        neurons={neurons}
+        connections={connections}
+        width={windowWidth}
+        height={windowHeight}
+      />
     </>
   )
 }
